9f85b8a9fe4ca622dc6f3f90dbd0d3a9ea30fbaf
Ngyyen <ngyyenle47@gmail.com>
2023-04-08 00:55:35 +0700

    add Xuly to private

--- BT_vi_du/Hoc_sinh.h
+++ BT_vi_du/Hoc_sinh.h
@@ -4,23 +4,28 @@
 using namespace std;
 
 class Hoc_sinh {
-public:
-	void Nhap();
-	void Xuat();
 private:
 	int MSSV;
 	string ho_ten;
 	double toan;
 	double van;
 	double diemtb;
+	void Xuly();
+
+public:
+	void Nhap();
+	void Xuat();
 };
+
+void Hoc_sinh::Xuly() {
+	diemtb = (toan + van) / 2;
+}
 
 void Hoc_sinh::Nhap() {
 	cin >> MSSV;
 	getline(cin, ho_ten);
 	cin >> toan;
 	cin >> van; 
-	diemtb = (toan + van) / 2;
 }
 
 void Hoc_sinh::Xuat() {
